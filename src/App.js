faff86ef3502e12f9b34ce33f86f359b74ae2bb8
Lam <Alice_Lam@Dell.com>
2018-08-06 14:58:51 -0500

    removing image and editing tab info

--- src/App.js
+++ src/App.js
@@ -13,24 +13,28 @@ class App extends Component {
 		        </div>
 		    <div id="menu">
 			    <ul>
-				    <li class="current_page_item"><a href="#" accesskey="1" title="">About</a></li>
-				    <li><a href="#" accesskey="2" title="">Resume</a></li>
-				    <li><a href="#" accesskey="3" title="">Projects</a></li>
-				    <li><a href="#" accesskey="4" title="">Contact</a></li>
+				    <li class="current_page_item"><a href="/#" accesskey="1" title="">Home</a></li>
+            <li><a href="/about" accesskey="2" title="">About</a></li>
+				    <li><a href="/resume" accesskey="3" title="">Resume</a></li>
+				    <li><a href="/projects" accesskey="4" title="">Projects</a></li>
+				    <li><a href="/contact" accesskey="5" title="">Contact</a></li>
 			    </ul>
 		    </div>
 	  </div>
 </div>
-
-      <div id="header-featured">
+  {/* <div id="header-featured"></div> */}
+  <div id="wrapper">
+	  <div id="page" class="container">
+		  <div id="content">
+			  <div class="title">
+				  <h2>Welcome to my website.</h2>
+				  <span class="byline">Mauris vulputate dolor sit amet nibh</span> </div>
+			    <p>This is <strong>Clear-Cut</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. All photographs on this site are my own. Source code for this project can be found  <a href="https://github.com/alice-lam/website" rel="nofollow">here</a>.</p>
+			    <a href="#" class="button">Explore Work.</a> 
+        </div>
+        </div>
       </div>
-	      <div id="banner-wrapper">
-		      <div id="banner" class="container">
-			      <h3>Hi. Welcome to my website.</h3>
-			      <span>Temporary placeholder text.</span>
-		      </div>
-	      </div>
-      </div>
+  </div>
     );
   }
 }
