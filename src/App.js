26942cead06a9eaa6b5f1e1d81d8b32f14e88686
Lam <Alice_Lam@Dell.com>
2018-08-06 11:54:36 -0500

    first template runthrough

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './fonts.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="header-wrapper">
+	        <div id="header" class="container">
+		        <div id="logo">
+			        <h1><a href="#">Alice Lam</a></h1>
+		        </div>
+		    <div id="menu">
+			    <ul>
+				    <li class="current_page_item"><a href="#" accesskey="1" title="">About</a></li>
+				    <li><a href="#" accesskey="2" title="">Resume</a></li>
+				    <li><a href="#" accesskey="3" title="">Projects</a></li>
+				    <li><a href="#" accesskey="4" title="">Contact</a></li>
+			    </ul>
+		    </div>
+	  </div>
+</div>
+
+      <div id="header-featured">
+      </div>
+	      <div id="banner-wrapper">
+		      <div id="banner" class="container">
+			      <h3>Hi. Welcome to my website.</h3>
+			      <span>Temporary placeholder text.</span>
+		      </div>
+	      </div>
       </div>
     );
   }
