fbcebb1283564722f8ee61e898571b581a0899ef
Lam <Alice_Lam@Dell.com>
2018-08-09 09:57:10 -0500

    about page setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import About from './views/About';
+
+ReactDOM.render(
+    <Router>
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/views/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import App from '../App';
+
+const About = () => (
+    <App>
+        <p>Test Text</p>                                
+    </App>
+    
+);
+
+export default About;
