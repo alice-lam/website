4ecce2342db34ee327fc62a8affbc08b66bbda5b
Lam, Alice <Alice_Lam@Dell.com>
2019-01-30 13:50:57 -0600

    add icon, fix responsive nav for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./styles/_header.scss";
 import Image from "./images/navbar-logo.png";
 
 class header extends Component {
   render() {
-    return(
-        <div className="navbar-container">
-            <img src={Image} alt="logo" className="navbar-logo"/>
-            <div className="navbar-menu">
-              <li className="navbar-menu-link">
-                <a href="">About</a>
-              </li>
-              <li className="navbar-menu-link">
-                <a href="">Resume</a>
-              </li>
-              <li className="navbar-menu-link">
-                <a href="">Projects</a>
-              </li>
-            </div>
+    return (
+      <div className="navbar-container">
+        <img src={Image} alt="logo" className="navbar-logo" />
+        <div className="navbar-menu">
+          <li className="navbar-menu-link">
+            <a href="">About</a>
+          </li>
+          <li className="navbar-menu-link">
+            <a href="">Resume</a>
+          </li>
+          <li className="navbar-menu-link">
+            <a href="">Projects</a>
+          </li>
         </div>
+        <a href="" className="icon" onClick="">
+          <i className="fas fa-bars"></i>
+        </a>
+      </div>
     );
-
   }
 }
 
-export default header; 
+export default header;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Resume from './views/Resume';
 ReactDOM.render(
     <Router>
       <Switch>
-        <Route path="/home" exact component={Home} />
+        <Route path="/" exact component={Home} />
         <Route path="/resume" component={Resume} />
       </Switch>
     </Router>, 
