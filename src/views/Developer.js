5bfc21ad9faf6f35dad99c3198f53408cf9e4099
unknown <alice94@sbcglobal.net>
2020-12-29 12:42:15 -0600

    home page update

--- src/views/Developer.js
+++ src/views/Developer.js
@@ -12,11 +12,11 @@ const Developer = () => (
         <section id="developer">
             <div id="content">
                 <h1 className="navbar-title"> DELLDESIGNSYSTEM.COM </h1>
-                <p>I'm currently working on the UI Core team at Dell Technologies
+                <p>I'm currently working on the Design Language System at Dell Technologies
                     to help develop a Design System that will be used across across Dell.
                 </p>
                 <p>   
-                    This project will help streamline all UI designs and promote consistency across all platforms. 
+                    This project helps streamline UI designs and promote consistency across all platforms. 
                 </p>
                 <a href="https://www.delldesignsystem.com"><img src={Image} alt="Dell Design System"></img></a>
             </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,7 +3,8 @@ import App from '../App';
 import './../App.scss';
 import Header from '../components/header.js';
 import Subheader from '../components/subheader.js';
-import Image from '../components/images/profile.png';
+import Image from '../components/images/home.png';
+import Signature from '../components/images/signature.png';
 
 const Home = () => (
     <App>
@@ -12,13 +13,19 @@ const Home = () => (
         <section id="home">
             <div id="content">
                 <div className="home-banner">
-                    <img src={Image} alt="profile"/>
+                    <img src={Image} className="banner-img" alt="profile"/>
+                    <div className="intro-container">
+                        <h1 className="title">Welcome to my site!</h1>
+                        <p>Hello, my name is Alice, nice to meet you! </p>
+                        <p>A little about myself: I graduated from UT Austin in 2017 with a BS in Electrical and Computer Engineering, with a technical focus on software development. I am currently working at Dell as a software engineer helping architect and build out Dell's Design Language System. I have back-end experience with OOP languages such as C++ and Java, as well as development experience with all major front-end frameworks. I am always trying to keep up with the latest tech, and am open to new opportunities to continue to learn and expand my skillset! </p>
+                        <p>Outside of work, I enjoy relaxing through hiking, reading, and playing games. I am also an active member Austin's Yelp Elite Community, and run a food instagram/blog on the side (@treatsbyalice), where I experiment with baking and food photography.</p>
+                        <br></br>
+                        <p>Thanks for taking the time to visit!</p>
+                        <p>Alice Lam</p>
+                        <img src={Signature} id="signature" alt="signature"/>
+                    </div>
                 </div>
-                <h1 className="title">Welcome to my website.</h1>
     
-                <p>Hi, my name is Alice, nice to meet you! </p>
-                <p>A little about myself: I graduated from UT Austin in 2017 with a BS in Electrical and Computer Engineering, with a technical focus on software development. I am currently working at Dell as a software engineer helping architect and build out Dell's Design Language System. I have development experience with both OOP languages such as C++ and Java, as well as experience with all front end frameworks; I am always open to new opportunities to learn and expand my skillset though! </p>
-                <p>Outside of work, I enjoy relaxing through hiking, reading, and playing games. I am also an active member Austin's Yelp Elite Community, and run a food instagram/blog on the side (@treatsbyalice), where I experiment with baking and food photography.</p>
             </div>
         </section>
     </App>
