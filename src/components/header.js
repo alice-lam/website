0b8d9d2e5690dc525ba5c85bdd26c297e7039858
unknown <alice94@sbcglobal.net>
2020-12-28 18:14:28 -0600

    updating mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,62 @@
 import React, { Component } from "react";
 import "./styles/_header.scss";
+import { NavLink } from 'react-router-dom';
 import Image from "./images/oldlogo.png";
 
 class header extends Component {
+  constructor(props){
+    super(props);
+    this.expanded = false;
+    this.handleMenu = this.handleMenu.bind(this);
+  }
+  componentDidMount(props){
+    this.menu = document.body.querySelector("div.navbar-mobile");
+  }
+  handleMenu(e) {
+    e.preventDefault();
+    if(e.target.classList.contains("menu-icon") || e.target.classList.contains("fa-bars")){
+      if(this.expanded){
+        this.closeMenu();
+      } else{
+        this.openMenu();
+      }
+      this.expanded = !this.expanded;
+    }
+  }
+  closeMenu(){
+    this.menu.classList.remove("show");
+  }
+  openMenu(){
+    this.menu.classList.add("show");
+  }
+  componentWillUnmount(props){
+    this.closeMenu();
+  }
+
   render() {
     return (
       <div className="navbar-container">
           <img src={Image} alt="logo" className="navbar-logo" />
           <h1 className="navbar-title"> A L I C E L A M . C O </h1>
-        <button href="" className="menu-icon">
-          <i className="fas fa-bars"></i>
+        <button className="menu-icon" onClick={this.handleMenu}>
+            <i className="fas fa-bars"></i>
         </button>
+        <div className="navbar-mobile">
+          <ul className="navbar-mobile-menu">
+            <li className="navbar-mobile-menu-item">
+              <NavLink exact activeClassName="active" to="/" href="/">Home</NavLink>
+            </li>
+            <li className="navbar-mobile-menu-item">
+              <NavLink exact activeClassName="active" to="/about" href="/about">About</NavLink>
+            </li>
+            <li className="navbar-mobile-menu-item">
+              <NavLink exact activeClassName="active" to="/developer" href="/developer">Developer Portfolio</NavLink>
+            </li>
+            <li className="navbar-mobile-menu-item">
+              <NavLink exact activeClassName="active" to="/treatsbyalice" href="/treatsbyalice">TreatsByAlice</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/views/Home.js
+++ src/views/Home.js
@@ -16,9 +16,9 @@ const Home = () => (
                 </div>
                 <h1 className="title">Welcome to my website.</h1>
     
-                <p>Hi, my name is Alice! I am currently a Software Engineer at Dell Technologies. I graduated from the University of Texas at Austin with a BS in Electrical and Computer Engineering, with a technical focus on software development. I have experience in object oriented programming such as C/C++ and Java, and also have experience with front-end development. I am always looking for opportunities where I can apply my skills as well as expand my knowledge.</p>
-                
-                <p>Outside of work, I am a pretty big foodie. I am an active member Austin's Yelp Elite Community, and run a food instagram/blog on the side, @treatsbyalice, where I experiment with baking and food photography.</p>
+                <p>Hi, my name is Alice, nice to meet you! </p>
+                <p>A little about myself: I graduated from UT Austin in 2017 with a BS in Electrical and Computer Engineering, with a technical focus on software development. I am currently working at Dell as a software engineer helping architect and build out Dell's Design Language System. I have development experience with both OOP languages such as C++ and Java, as well as experience with all front end frameworks; I am always open to new opportunities to learn and expand my skillset though! </p>
+                <p>Outside of work, I enjoy relaxing through hiking, reading, and playing games. I am also an active member Austin's Yelp Elite Community, and run a food instagram/blog on the side (@treatsbyalice), where I experiment with baking and food photography.</p>
             </div>
         </section>
     </App>
